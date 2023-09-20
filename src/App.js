f922c931acf101fc113bbd72b59c85378f6ec137
Matthew Jackson <120997192+mattyj7@users.noreply.github.com>
2023-09-20 17:36:43 +0000

    Added Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./styles.css";
+import Home from "./pages/Home";
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <Home />
     </div>
   );
 }
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Home() {
+    return (
+        <div>
+            <h2>Home Welcome</h2>
+        </div>
+    )
+}
+
+export default Home;
