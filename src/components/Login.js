1fdaee1a198d93fd089319bcb878c183d56c48f0
Matthew Jackson <120997192+mattyj7@users.noreply.github.com>
2023-09-21 15:45:47 +0000

    Updated Login.js and styles.css

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,18 @@
 const Login = () => {
     return (
         <div className="login-section">
-            <label> Username: </label>
-            <input></input>
-            <label>Password: </label>
-            <input></input>
+            <div className="username">
+                <label> Username: </label>
+                <input></input>
+            </div>
+            <div className="password">
+                <label> Password: </label>
+                <input></input>
+            </div>
+            <div className="login-btn">
+                <button>Login</button>
+                <button>Cancel</button>
+            </div>
         </div>
     )
 }
